d6e80083efc2dc5896fb9520a842f6cc4b5a9a1d
Asgeir Enersen <asgeir@enersen.com>
2014-08-30 18:27:08 +0000

    Added first basic setup tools.

---
+++ setupdirs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Proper header for a Bash script.
+
+# Setup necessary directories...
+mkdir ../source
+mkdir ../appengine_apps
+mkdir ../appengine_apps/styreapp_prod
+mkdir ../appengine_apps/styreapp_dev
