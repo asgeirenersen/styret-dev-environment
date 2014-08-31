45ad7d90f28c51bc3c058375496f7e0da495b24d
Asgeir Enersen <asgeir@enersen.com>
2014-08-31 21:49:05 +0000

    Updated update-dev.sh and the config files.

--- update-dev.sh
+++ update-dev.sh
@@ -4,3 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 rm -r ${DIR}/../appengine_apps/styreapp_dev/saksbehandling
 cp -r ${DIR}/../source/saksbehandling/ ${DIR}/../appengine_apps/styreapp_dev/
+cp -r ${DIR}/configs/google_drive_settings.dev.json ${DIR}/../appengine_apps/styreapp_dev/saksbehandling/google_drive_settings.json
