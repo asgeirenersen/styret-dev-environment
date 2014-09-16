a78de6fe5b34620b4737d043f0c1f9a11906f55b
Asgeir Enersen <asgeir@enersen.com>
2014-09-16 20:39:29 +0000

    Now update-app.sh copies over app.yaml and inserts the correct app name.

--- update-app.sh
+++ update-app.sh
@@ -4,6 +4,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ENV=$1
 
-rm -r ${DIR}/../appengine_apps/styreapp_${ENV}/saksbehandling
-cp -r ${DIR}/../source/saksbehandling/ ${DIR}/../appengine_apps/styreapp_${ENV}/
+if [ $ENV = "prod" ] ; then
+    APP="styret-hallagerbakken"
+else
+    APP="test-hallagerbakken"
+fi
+
+rm -r ${DIR}/../appengine_apps/styreapp_${ENV}/*
+cp -r ${DIR}/../source/* ${DIR}/../appengine_apps/styreapp_${ENV}/
 cp -r ${DIR}/configs/google_drive_settings.${ENV}.json ${DIR}/../appengine_apps/styreapp_${ENV}/saksbehandling/google_drive_settings.json
+
+sed -i -e "s/__APP_NAME__/${APP}/g" ${DIR}/../appengine_apps/styreapp_${ENV}/app.yaml
